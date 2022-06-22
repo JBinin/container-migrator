02c5c3e04995a64844ea0393f34ec3644dd6b865
JBinin <namechenjiabin@icloud.com>
2022-06-23 00:40:41 +0800

    clean client code

--- server/server.go
+++ server/server.go
@@ -10,8 +10,12 @@ import (
 )
 
 func ListenAndServe(migratedContainerDir string) {
-	os.RemoveAll(migratedContainerDir)
-	os.MkdirAll(migratedContainerDir, os.ModePerm)
+	if err := os.RemoveAll(migratedContainerDir); err != nil {
+		log.Println("Failed to remove ", migratedContainerDir)
+	}
+	if err := os.MkdirAll(migratedContainerDir, os.ModePerm); err != nil {
+		log.Println("Failed to mkdir ", migratedContainerDir)
+	}
 
 	conn, err := net.Listen("tcp", ":8001")
 	defer conn.Close()
@@ -20,51 +24,65 @@ func ListenAndServe(migratedContainerDir string) {
 		return
 	}
 	for {
-		acc, err1 := conn.Accept()
-		if err1 != nil {
-			log.Println(err1.Error())
+		if acc, err := conn.Accept(); err != nil {
+			log.Println(err.Error())
 			break
+		} else {
+			go handleConn(acc, migratedContainerDir)
 		}
-		go handleConn(acc, migratedContainerDir)
 	}
 }
 
 func handleConn(c net.Conn, migratedContainerDir string) {
 	defer c.Close()
-
-	f, err := os.OpenFile("server.log", os.O_CREATE|os.O_APPEND|os.O_RDWR, os.ModePerm)
-	if err != nil {
-		return
+	if f, err := os.OpenFile("server.log", os.O_CREATE|os.O_APPEND|os.O_RDWR, os.ModePerm); err != nil {
+		log.Println("Failed to open the server.log")
+	} else {
+		defer f.Close()
+		log.SetOutput(f)
 	}
-	defer f.Close()
-	log.SetOutput(f)
 
 	var buf [512]byte
-	n, err1 := c.Read(buf[:])
-	if err1 != nil {
-		log.Fatal(err1)
+	if n, err := c.Read(buf[:]); err != nil {
+		log.Println("Failed to receive DestPath cmd")
+		return
+	} else {
+		receive := string(buf[:n])
+		log.Println(receive)
+		if receive == "DestPath" {
+			if _, err := c.Write([]byte(migratedContainerDir)); err != nil {
+				log.Println("Failed to send the migratedContainerDir to client")
+				return
+			}
+		}
 	}
-	receive := string(buf[:n])
-	log.Println(receive)
-	if receive == "DestPath" {
-		c.Write([]byte(migratedContainerDir))
-	}
-	n, err1 = c.Read(buf[:])
-	if err1 != nil {
-		log.Fatal(err1)
-	}
-	receive = string(buf[:n])
-	log.Println(receive)
-	if strings.HasPrefix(receive, "restore") {
-		cmd := strings.Split(receive, ":")
-		imagePath := path.Join(migratedContainerDir, "checkpoint")
-		args := []string{cmd[0], "--image-path", imagePath, cmd[1]}
 
-		oldDir, _ := os.Getwd()
-		os.Chdir(migratedContainerDir)
-		exec.Command("runc", args...).Output()
-		os.Chdir(oldDir)
-		c.Write([]byte("started"))
+	if n, err := c.Read(buf[:]); err != nil {
+		log.Println("Failed to receive restore cmd")
+		return
+	} else {
+		receive := string(buf[:n])
+		log.Println(receive)
+		if strings.HasPrefix(receive, "restore") {
+			cmd := strings.Split(receive, ":")
+			imagePath := path.Join(migratedContainerDir, "checkpoint")
+			args := []string{cmd[0], "--image-path", imagePath, cmd[1]}
+
+			oldDir, _ := os.Getwd()
+			os.Chdir(migratedContainerDir)
+			defer os.Chdir(oldDir)
+
+			if _, err := exec.Command("runc", args...).Output(); err != nil {
+				log.Println("Failed to restore the contaier")
+				log.Println(err.Error())
+				return
+			} else {
+				if _, err := c.Write([]byte("started")); err != nil {
+					log.Println("Failed to send the started message to client ")
+					return
+				}
+			}
+		}
 	}
 	log.Println("Handle finished.")
 }
