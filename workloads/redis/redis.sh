392793a347867918c90096a035d981418cf534d1
JBinin <namechenjiabin@icloud.com>
2022-06-23 22:23:40 +0800

    migrator path

--- workloads/redis/redis.sh
+++ workloads/redis/redis.sh
@@ -1,3 +1,3 @@
 runc run myredis > /dev/null &
 sleep 4
-./migrator client  --container_id myredis --destination 172.31.92.143 --others_path ${PWD}/redis --expected_time 1 pre_copy
+../../migrator client  --container_id myredis --destination 172.31.92.143 --others_path ${PWD}/redis --expected_time 1 pre_copy
