837fed8ffd2fd97db78ff8a027ad5053bf1fbf09
JBinin <namechenjiabin@icloud.com>
2022-06-24 23:59:36 +0800

    start

--- workloads/redis/data/start.sh
+++ workloads/redis/data/start.sh
@@ -1,3 +1,3 @@
 redis-server --bind 0.0.0.0  &
 sleep 2
-redis-benchmark -t SET -c 20 -n 3000000 -r 10000000 -d 2048  > /dev/null
+redis-benchmark -t SET -c 10 -n 1000000 -r 10000000 -d 1024  > /dev/null
