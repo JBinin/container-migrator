0f489da19ac84c5bad92389fe97832fb6e668f1b
JBinin <namechenjiabin@icloud.com>
2022-06-24 18:09:04 +0800

    redis.sh more wait time

--- workloads/redis/redis.sh
+++ workloads/redis/redis.sh
@@ -1,5 +1,5 @@
 cd redis
 runc run myredis > /dev/null &
 cd ../
-sleep 10
+sleep 15
 ../migrator client  --container_id myredis --destination 172.31.14.127 --others_path ${PWD}/redis --expected_time 1 pre_copy
