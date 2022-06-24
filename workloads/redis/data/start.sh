18b1bf0e5cb8cb47792e7ef8397b6f5483a2bde6
root <root@ip-172-31-9-142.ec2.internal>
2022-06-24 15:20:16 +0000

    start and redis

--- workloads/redis/data/start.sh
+++ workloads/redis/data/start.sh
@@ -1,3 +1,3 @@
 redis-server --bind 0.0.0.0  &
 sleep 2
-redis-benchmark -t SET -c 10 -n 10000000 -r 10000000 -d 32  > /dev/null
+redis-benchmark -t SET -c 20 -n 3000000 -r 10000000 -d 2048  > /dev/null
--- workloads/redis/redis.sh
+++ workloads/redis/redis.sh
@@ -1,5 +1,6 @@
 cd redis
 runc run myredis > /dev/null &
 cd ../
-sleep 15
+sleep 20
+echo "start migrator"
 ../migrator client  --container_id myredis --destination 172.31.14.127 --others_path ${PWD}/redis --expected_time 1 pre_copy
