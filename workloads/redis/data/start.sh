0873da74e8e12e66e74e294cd0c3730d9d8ecb48
JBinin <namechenjiabin@icloud.com>
2022-07-01 16:45:55 +0800

    set 500M in total

--- workloads/redis/data/start.sh
+++ workloads/redis/data/start.sh
@@ -1,3 +1,3 @@
 redis-server --bind 0.0.0.0  &
 sleep 2
-redis-benchmark -t SET -c 10 -n 1000000 -r 10000000 -d 1024  > /dev/null
+redis-benchmark -t SET -c 10 -n 500000 -r 10000000 -d 1024  > /dev/null
