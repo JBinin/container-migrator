7ab544ccbbb93f47eee4b0f71195c115a5bf4cc8
root <root@ip-172-31-9-142.ec2.internal>
2022-07-01 12:37:14 +0000

    redis conf

--- workloads/redis/data/start.sh
+++ workloads/redis/data/start.sh
@@ -1,3 +1,3 @@
-redis-server --bind 0.0.0.0  &
+redis-server /data/redis.conf --bind 0.0.0.0  &
 sleep 2
-redis-benchmark -t SET -c 10 -n 500000 -r 10000000 -d 1024  > /dev/null
+redis-benchmark -t SET -c 10 -n 10000000 -r 10000000 -d 128  > /dev/null
