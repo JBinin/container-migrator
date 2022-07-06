974daf816ec79b083e2e5e7bbb7e26c0a05bfb1d
JBinin <namechenjiabin@icloud.com>
2022-07-06 22:54:23 +0800

    test modify

--- workloads/redis/test.sh
+++ workloads/redis/test.sh
@@ -1,6 +1,6 @@
 cd redis
 runc run testredis > /dev/null &
 cd ../
-sleep 30
+sleep 20
 echo "start test"
 ../migrator predump-only  --container_id testredis  --checkpoint_path ${PWD}/image/testredis
